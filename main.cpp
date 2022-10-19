58fe9f0bc77f899ce85051e9ef4f8c85ea8f10e4
Surya Gempita <95684526+CaptKeroro@users.noreply.github.com>
2022-10-19 18:41:33 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  float nilaiA, nilaiB;
+  char operators;
+
+  cout << "masukan nilai a : ";
+  cin >> nilaiA;
+  cout << "masukan nilai b : ";
+  cin >> nilaiB; 
+  cout << "pilih operator, (contoh = +, -, /, *, %) : ";
+  cin >> operators;
+  if (operators == '+') {
+    cout << "pertambahan" << endl;
+    cout << "a + b = "<< nilaiA + nilaiB << endl;
+  }
+  else if (operators == '-') {
+    cout << "perkurangan" << endl;
+    cout << "a - b = "<< nilaiA - nilaiB << endl;
+  }
+  else if (operators == '/') {
+    cout << "pembagian" << endl;
+    cout << "a / b = "<< nilaiA / nilaiB << endl;
+  }
+  else if (operators == '*') {
+    cout << "perkalian" << endl;
+    cout << "a * b = "<< nilaiA * nilaiB << endl;
+  }
+  else if (operators == '%') {
+    cout << "sisa bagi (modulus)" << endl;
+    cout << "a % b = "<< (int(nilaiA) % int(nilaiB)) << endl;
+  }
+  else {
+    cout << "operator todak tersedia";
+  }
+
+  return 0;
+}
